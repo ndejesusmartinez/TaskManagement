bc2cce174eb5deeb45ce446be1070cf117e0cd52
dev_naren09 <desarrollo09@domina.com.co>
2023-12-21 12:56:37 -0500

    Ajustes finales

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -3,22 +3,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     <title>Register</title>
 </head>
 <body>
 
+<div class="container">
+    <form action="{{ route('RegisterUser') }}" method="post">
+        @csrf
+        <div class="mb-3">
+            <input class="form-control" type="text" name="name" id="name" placeholder="Nombre">
+        </div>
+        <div class="mb-3">
+            <input class="form-control" type="email" name="email" id="email" placeholder="Correo">
+        </div>
+        <div class="mb-3">
+            <input class="form-control" type="password" name="password" id="password" placeholder="Contraseña">
+        </div>
+        <button class="btn btn-primary" type="submit">Registrar</button>
+    </form>
+</div>
 
-<form action="{{ route('RegisterUser') }}" method="post">
-    @csrf
-    <input type="text" name="name" id="name" placeholder="Nombre">
-    <input type="email" name="email" id="email" placeholder="Correo">
-    <input type="password" name="password" id="password" placeholder="Contraseña">
-
-    <button type="submit">Registrar</button>
-</form>
-
-
-<a href="{{ route('login') }}"> <button>Estas registrado, inicia sesión</button></a>
+<a class="btn btn-primary" href="{{ route('login') }}"> <button>Estas registrado, inicia sesión</button></a>
 
 </body>
 </html>
